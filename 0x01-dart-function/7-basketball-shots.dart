643c122fb81dcaaf6f5b5ed71274f04267f4c909
cocofox1902 <colas.renard1902@gmail.com>
2023-07-18 19:23:31 +0200

    add all

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -13,8 +13,8 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
 
 int calculatePoints(Map<String, int> team) {
   int freeThrows = team['Free throws'] ?? 0;
-  int twoPointers = team['2 pointer'] ?? 0;
-  int threePointers = team['3 pointer'] ?? 0;
+  int twoPointers = team['2 pointers'] ?? 0;
+  int threePointers = team['3 pointers'] ?? 0;
   
   int totalPoints = freeThrows + (twoPointers * 2) + (threePointers * 3);
   
